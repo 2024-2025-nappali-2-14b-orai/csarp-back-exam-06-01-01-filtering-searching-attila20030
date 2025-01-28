220cd329bc5afde43fc0d6bd32c76986fc995a47
szt3daratt <szt3daratt@SZT5-3-HP>
2025-01-28 09:39:14 +0100

    Újabb lekérdezések

--- Kreata.Backend/Controllers/StudentController.cs
+++ Kreata.Backend/Controllers/StudentController.cs
@@ -20,6 +20,37 @@ namespace Kreata.Backend.Controllers
         {
             return Ok(await _studentRepo.GetByNameAsync(fullNameDto.FirstName, fullNameDto.LastName));
         }
+
+        [HttpGet("NumberOfStudent")]
+        public async Task<IActionResult> GetNumberOfStudentsAsync()
+        {
+            return Ok(await _studentRepo.GetNumberOfStudentAsync());
+        }        
+        
+        [HttpGet("NumberOfWoman")]
+        public async Task<IActionResult> GetNumberOfWomanAsync()
+        {
+            return Ok(await _studentRepo.GetNumberOfWomanAsync());
+        }        
+        
+        
+        [HttpGet("NumberOfMan")]
+        public async Task<IActionResult> GetNumberOfManAsync()
+        {
+            return Ok(await _studentRepo.GetNumberOfManAsync());
+        }        
+        
+        [HttpGet("BornInTwentyTwentyOne")]// A diákok száma akik 2021-ben születtek
+        public async Task<IActionResult> GetNumberOfStudentByBirthdayAsync()
+        {
+            return Ok(await _studentRepo.GetNumberOfStudentByBirthdayAsync());
+        }        
+        
+        [HttpGet("GetNumberOfStudentBornInAprilAsync")]// A diákok száma akik áprilisban születtek
+        public async Task<IActionResult> GetNumberOfStudentBornInAprilAsync()
+        {
+            return Ok(await _studentRepo.GetNumberOfStudentBornInAprilAsync());
+        }
     }
 
 }
